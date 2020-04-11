1d6893aa03aeb92eeb9f0e996156f39e16dde07e
zibrulyaa <pzibrov@icloud.com>
2020-04-11 17:54:49 +0300

    added product-slider

--- app/js/main.js
+++ app/js/main.js
@@ -3,4 +3,13 @@ $(function(){
         arrows: false,
         autoplay: 5000
     });
+    $('.rateyo').rateYo({
+        rating: 5,
+        starWidth: '15px',
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        slidesToScroll:1,
+        slidesToShow: 1
+    });
 })
--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,8 @@ gulp.task("style", function() {
     .src([
       "node_modules/slick-carousel/slick/slick.css",
       'node_modules/normalize.css/normalize.css',
-      "node_modules/magnific-popup/dist/magnific-popup.css"
+      "node_modules/magnific-popup/dist/magnific-popup.css",
+      "node_modules/rateyo/src/jquery.rateyo.css"
     ])
     .pipe(concat("libs.min.css"))
     .pipe(cssmin())
@@ -36,7 +37,8 @@ gulp.task("script", function() {
   return gulp
     .src([
       "node_modules/slick-carousel/slick/slick.js",
-      "node_modules/magnific-popup/dist/jquery.magnific-popup.js"
+      "node_modules/magnific-popup/dist/jquery.magnific-popup.js",
+      "node_modules/rateyo/src/jquery.rateyo.js"
     ])
     .pipe(concat("libs.min.js"))
     .pipe(uglify())
