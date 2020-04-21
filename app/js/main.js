64a61018be07d088013bf5f4a056f00d7431785a
zibrulyaa <pzibrov@icloud.com>
2020-04-21 16:32:30 +0300

    added asidebar, product grid

--- app/js/main.js
+++ app/js/main.js
@@ -1,21 +1,29 @@
-$(function(){
-    $('.slider__inner').slick({
-        arrows: false,
-        autoplay: 5000
-    });
-    $('.rateyo').rateYo({
-        rating: 5,
-        starWidth: '15px',
-        readOnly: true
-    });
-    $('.product-slider__inner').slick({
-        slidesToScroll:1,
-        slidesToShow: 1
-    });
-    $('.followers-slider__inner').slick({
-        slidesToShow: 3,
-        slidesToScroll: 3,
-        arrows: true
-    });
-    var mixer = mixitup('.mix__products-inner');
-})
+$(function () {
+  $(".slider__inner").slick({
+    arrows: false,
+    autoplay: 5000,
+  });
+  $(".rateyo").rateYo({
+    rating: 5,
+    starWidth: "15px",
+    readOnly: true,
+  });
+  $(".product-slider__inner").slick({
+    slidesToScroll: 1,
+    slidesToShow: 1,
+  });
+  $(".followers-slider__inner").slick({
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+  });
+  var mixer = mixitup(".mix__products-inner");
+});
+$(".js-range-slider").ionRangeSlider({
+  type: "double",
+  min: 0,
+  max: 320,
+  from: 30,
+  to: 300,
+  prefix: "$",
+});
